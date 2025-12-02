a272d3ca3f4facdebf17292711394a1a05ac2dca
Kurt Bixby <kurtbixby@gmail.com>
2025-12-01 23:03:03 -0600

    Day 1.2 done

--- day01/index.js
+++ day01/index.js
@@ -8,25 +8,68 @@ function setup() {
     rl.on("line", (line) => {
         const direction = line[0];
         const rawValue = parseInt(line.substring(1));
-        const adjustedValue = direction === 'R' ? rawValue : 100 - (rawValue % 100);
-        values.push(adjustedValue);
+        const signedValue = direction === 'R' ? rawValue : -rawValue;
+        values.push(signedValue);
     });
 
     rl.once("close", () => main(values));
 }
 
 function main(values, start = 50) {
-    const { position, count } = values.reduce((acc, val) => {
-        const newPosition = Math.abs((acc.position + val) % 100);
-        if (newPosition === 0 && val !== 0) {
-            acc.count += 1;
+    const { count, midCount } = values.reduce((acc, val) => {
+
+        let remaining = val;
+        let position = acc.position;
+        while (remaining !== 0) {
+            if (remaining < 0) {
+                if (remaining < -100) {
+                    remaining += 100;
+                    acc.midCount++;
+                } else {
+                    if (position !== 0 && position + remaining < 0) {
+                        acc.midCount++;
+                    }
+                    position = trueMod(position + remaining, 100);
+                    remaining = 0;
+                }
+            } else {
+                if (remaining > 100) {
+                    remaining -= 100;
+                    acc.midCount++;
+                } else {
+                    if (position !== 0 && position + remaining > 100) {
+                        acc.midCount++;
+                    }
+                    position = (position + remaining) % 100;
+                    remaining = 0;
+                }
+            }
         }
-        console.debug("Cur: %d, Add: %d, Res: %d", acc.position, val, newPosition)
-        acc.position = newPosition;
+
+        if (position === 0) {
+            acc.count++;
+            acc.midCount++;
+        }
+        
+        acc.position = position;
         return acc;
-    }, {position: start, count: 0})
+    }, {position: start, count: 0, midCount: 0})
 
     console.log(count);
+    console.log(midCount);
+}
+
+function trueMod(integer, modulus) {
+    if (integer >= 0) {
+        return integer % modulus;
+    }
+
+    let shrunkInt = integer;
+    while (Math.abs(shrunkInt) > modulus) {
+        shrunkInt += modulus;
+    }
+
+    return modulus + shrunkInt;
 }
 
 setup();
